85b106dc42dbdbb35ffedc29ab6848dda8378a80
Aditi Green <asaki444@gmail.com>
2017-04-20 18:19:17 +0000

    i had to commit my changes to checkout Nikki's branch

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,6 +1,7 @@
 require 'pry'
 class Game
   attr_accessor :board, :player_1, :player_2
+  attr_reader
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O") , board = Board.new)
     @board =  board
     @player_1 = player_1
--- lib/Players.rb
+++ lib/Players.rb
@@ -9,17 +9,16 @@ class Players
   class Computer < Player
     attr_accessor :available_moves, :pos_taken, :board
 
-    def move(board)
-      #we can access to this board in any function with self.board_Niki
-
-      board.turn_count <= 5 ? Random.rand(0..9).to_s : self.strategy
-
-    end
+      def move(board)
+        #we can access to this board in any function with self.board_Niki
+        @board = board
+        board.turn_count <= 4 ? Random.rand(0..9).to_s : self.strategy.to_s
+        end
  # maybe it would be better to divide this function (strategy -> search_moves + strategy)_Niki
     def search_moves
-      self.available_moves = []
-      self.pos_taken = []
-      self.board.cells.each.with_index do |val,index|
+      @available_moves = []
+      @pos_taken = []
+      board.cells.each.with_index do |val,index|
         if val == " "
           self.available_moves << index
         elsif val == self.token
@@ -29,12 +28,36 @@ class Players
     end
 
     def strategy
-      self.board.WIN_COMBINATIONS.each do |win_combination|
-        win_combination.detect.with_index do |x,i|
-          x == self.token && self.available_moves.include?(x)
-          #wouldnt a nested detect would give us a single index at a time. so I just compared x to the token, and asked if the spot is available.
-        end
+      i = 0
+      move = ""
+      def win
+        Game::WIN_COMBINATIONS.each do |win_combination|
+          array = win_combination.find_all{|x| x == token}
+             if array.size == 2
+              move  << win_combination.reject{|x| x == array[0] || x == array[1] || x == array [2]}.to_s
+            end
+          end
+        move
       end
+        if win(self.token) == " "
+            win("other players token")
+        else
+          win
+        end
+
+        # win_combination.detect do |win_index|
+        #  #checks for the position taken of the opponent and the computer
+        #  #blocks if it is about to win
+        # end
+        # win_combination.detect.with_index do |x,i|
+        #   if x == self.token && @available_moves.include?(i+2) && board.cells[i+1] == self.token
+        #       i+1
+        #   elsif x == self.token && @available_moves.include?(i+1) && board.cells[i+2] == self.token
+        #       i+2
+        #   end
+        #   #wouldnt a nested detect would give us a single index at a time. so I just compared x to the token, and asked if the spot is available.
+
+       end
     end
         #i want to use the WIN_COMBINATIONS array to detect our next best move to win.
         #checking if the token is occupying two out of three winning spots, and hopefully returning the move
@@ -42,6 +65,6 @@ class Players
         #I think we should use yield twice because the WIN_COMBINATIONS is nested array. However, the IDEA is GREAT! _Niki
 
   #binding.pry
-  end
+
 
 end
